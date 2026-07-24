a8dfd2d4862418edee40e3f38d77cc7538eb1476
sudosubin <sudosubin@gmail.com>
2026-07-25 02:09:52 +0900

    fix: Storybook 브라우저 테스트 cold-start 대응(optimizeDeps)

--- vite.config.ts
+++ vite.config.ts
@@ -65,6 +65,9 @@ export default defineConfig({
       },
       {
         extends: true,
+        optimizeDeps: {
+          include: ["@vanilla-extract/recipes/createRuntimeFn"],
+        },
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
