b555b0fa6b1ec0c56e71ae45a6da95b88f74231d
Subin Kim <sudosubin@gmail.com>
2026-07-25 01:23:23 +0900

    feat: TanStack Query provider 및 devtools 설정 (#5)

---
+++ src/providers/query-provider/query-client.ts
@@ -0,0 +1,12 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 60_000,
+    },
+  },
+});
