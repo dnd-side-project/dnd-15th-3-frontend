61c750c3d736afb1c201cad28b82c2fd24093d5f
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2026-08-06 19:10:47 +0900

    feat: 모임 날짜 선택 컴포넌트 구현 (#25)
    * feat: react-day-picker 설치
    
    * feat: 컴포넌트에 사용될 아이콘 추가(트리거, 캘린더)
    
    * feat: 모임 날짜 선택 컴포넌트 구현
    
    * feat: 월 기본 값 선택된 날짜의 월로 설정
    
    * feat: 사용되지 않는orientation일 경우 기존 Chevron 반환하게 수정
    
    * feat: 확인 버튼 너비 기기 너비에 따라 변하게 수정
    
    * feat: 캘린더 오버플로우 처리
    
    * feat: 버튼 커서 스타일 추가
    
    * feat: 확인 버튼 외부 여백을 래퍼 추가

---
+++ src/domains/new/components/day-picker/index.tsx
@@ -0,0 +1,88 @@
+import { DayPicker as ReactDayPicker } from "@daypicker/react";
+import { Chevron as RdpChevron } from "@daypicker/react";
+import { ko } from "@daypicker/react/locale";
+import { useState, type Dispatch, type SetStateAction, type ComponentProps } from "react";
+
+import CalendarIcon from "../../../../assets/icon-calendar.svg?react";
+import CaretLeftIcon from "../../../../assets/icon-caret-left.svg?react";
+import CaretRightIcon from "../../../../assets/icon-caret-right.svg?react";
+import { BottomSheet } from "../../../../components/bottom-sheet";
+import { formatTwoDigits } from "../../utils/time";
+
+import * as styles from "./index.css";
+
+interface DayPickerProps {
+  date: Date | undefined;
+  setDate: Dispatch<SetStateAction<Date | undefined>>;
+}
+
+function Chevron({ orientation, ...props }: ComponentProps<typeof RdpChevron>) {
+  if (orientation === "left") {
+    return <CaretLeftIcon width={20} height={20} />;
+  }
+  if (orientation === "right") {
+    return <CaretRightIcon width={20} height={20} />;
+  }
+  return <RdpChevron orientation={orientation} {...props} />;
+}
+
+export function DayPicker({ date, setDate }: DayPickerProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [inputDate, setInputDate] = useState<Date | undefined>(date);
+  const dateString = date
+    ? `${formatTwoDigits(date.getFullYear() % 100)}.${formatTwoDigits(date.getMonth() + 1)}.${formatTwoDigits(date.getDate())}`
+    : "YY.MM.DD";
+  const handleClickConfirmButton = () => {
+    setDate(inputDate);
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <button className={styles.trigger} onClick={() => setIsOpen(!isOpen)}>
+        <CalendarIcon width={24} height={24} />
+        {dateString}
+      </button>
+      <BottomSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div className={styles.dayPickArea}>
+          <ReactDayPicker
+            className={styles.calendar}
+            classNames={{
+              month_caption: styles.monthCaption,
+              weekdays: styles.weekdays,
+              weekday: styles.weekday,
+              nav: styles.navigation,
+              month_grid: styles.monthGrid,
+              day: styles.day,
+              day_button: styles.dayButton,
+              button_previous: styles.navigationButton,
+              button_next: styles.navigationButton,
+              selected: styles.selectedDay,
+              today: "",
+              outside: styles.outside,
+            }}
+            locale={ko}
+            mode="single"
+            selected={inputDate}
+            onSelect={setInputDate}
+            components={{
+              Chevron: Chevron,
+            }}
+            formatters={{
+              formatCaption: (date) => `${date.getFullYear()} ${date.getMonth() + 1}월`,
+            }}
+            defaultMonth={date}
+          />
+          <div className={styles.confirmButtonWrapper}>
+            <button
+              className={styles.confirmButton({ tone: inputDate ? "primary" : "secondary" })}
+              onClick={handleClickConfirmButton}
+              disabled={!inputDate}
+            >
+              확인
+            </button>
+          </div>
+        </div>
+      </BottomSheet>
+    </>
+  );
+}
