f65500b203254f85f81520c01cbb576df561705a
sudosubin <sudosubin@gmail.com>
2026-08-10 02:54:58 +0900

    feat: 홈 페이지 구현

---
+++ src/domains/home/pages/index.css.ts
@@ -0,0 +1,146 @@
+import { style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
+
+import { vars } from "../../../styles/theme.css";
+
+const colors = {
+  logo: "#3592FF",
+  title: "#3D3D3D",
+  description: "#707D91",
+  cardBackground: "#ECEFF5",
+  cardTitle: "#3793FF",
+  arrowBackground: "rgba(0, 0, 0, 0.1)",
+  arrowIcon: "#FFFFFF",
+};
+
+export const root = style({
+  display: "flex",
+  flexDirection: "column",
+  padding: "20px 20px 0",
+});
+
+export const logo = style({
+  display: "block",
+  color: colors.logo,
+});
+
+export const intro = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: 8,
+  marginTop: 81,
+  padding: "0 3px",
+});
+
+export const title = style({
+  margin: 0,
+  color: colors.title,
+  fontFamily: vars.font.body,
+  fontSize: 24,
+  fontWeight: 600,
+  lineHeight: 1.2,
+});
+
+export const description = style({
+  margin: 0,
+  color: colors.description,
+  fontFamily: vars.font.body,
+  fontSize: 16,
+  fontWeight: 500,
+  lineHeight: 1.2,
+});
+
+export const cards = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: 18,
+  marginTop: 47,
+});
+
+export const card = recipe({
+  base: {
+    position: "relative",
+    display: "block",
+    overflow: "hidden",
+    borderRadius: 12,
+    backgroundColor: colors.cardBackground,
+    boxShadow: "0 0 2.1px rgba(0, 0, 0, 0.25)",
+    textDecoration: "none",
+  },
+  variants: {
+    size: {
+      large: { height: 254 },
+      small: { height: 79 },
+    },
+  },
+});
+
+export const confetti = style({
+  position: "absolute",
+  top: -120,
+  left: -49.26,
+  width: 450.8,
+  height: 256.93,
+});
+
+export const illustration = recipe({
+  base: { position: "absolute" },
+  variants: {
+    size: {
+      large: { top: 21, left: 34, width: 319, height: 233 },
+      small: { top: 0, left: 137, width: 210, height: 79 },
+    },
+  },
+});
+
+export const cardContent = recipe({
+  base: {
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 300,
+    marginLeft: 25,
+  },
+  variants: {
+    size: {
+      large: { marginTop: 16 },
+      small: { marginTop: 14 },
+    },
+  },
+});
+
+export const cardTexts = style({
+  display: "flex",
+  flexDirection: "column",
+});
+
+export const cardTitle = style({
+  margin: 0,
+  color: colors.cardTitle,
+  fontFamily: vars.font.body,
+  fontSize: 18,
+  fontWeight: 600,
+  lineHeight: 1.65,
+});
+
+export const cardDescription = style({
+  margin: "-2px 0 0",
+  color: colors.description,
+  fontFamily: vars.font.body,
+  fontSize: 14,
+  fontWeight: 500,
+  lineHeight: 1.65,
+});
+
+export const arrow = style({
+  display: "flex",
+  flexShrink: 0,
+  alignItems: "center",
+  justifyContent: "center",
+  width: 28,
+  height: 28,
+  borderRadius: vars.radius.full,
+  backgroundColor: colors.arrowBackground,
+  color: colors.arrowIcon,
+});
