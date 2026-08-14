100a3b47b60a957ad6f8ae4ad703efe9cca800f8
sudosubin <sudosubin@gmail.com>
2026-08-14 13:36:02 +0900

    refactor: 환경변수 선언을 이름순으로 정렬

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL?: string;
   readonly VITE_KAKAO_JS_KEY: string;
   readonly VITE_KAKAO_MAP_KEY: string;
-  readonly VITE_API_BASE_URL?: string;
 }
 
 interface ImportMeta {
