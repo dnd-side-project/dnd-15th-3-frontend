7473c9405d684172bf5930ebb8eecc8aa76dbc9a
sudosubin <sudosubin@gmail.com>
2026-08-14 22:24:40 +0900

    refactor: 공용 헬퍼 경로를 utils 로 맞춤

--- src/components/momo-avatar/index.tsx
+++ src/components/momo-avatar/index.tsx
@@ -1,5 +1,5 @@
 import type { ProfileAvatarId } from "../../domains/catalog/api/types";
-import { cx } from "../../lib/cx";
+import { cx } from "../../utils/cx";
 
 import { image, root } from "./index.css";
 
